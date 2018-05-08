a1619cdd4d9b4a0bf28a832d6b26911f7d2bdcef
unknown <jempsey@gmail.com>
2018-05-08 16:23:22 -0700

    finished

--- src/components/Post/Edit/Edit.js
+++ src/components/Post/Edit/Edit.js
@@ -20,13 +20,19 @@ export default class Edit extends Component {
   }
 
   updatePost() {
+    const { text } = this.state;
+    const { id, updatePostFn, hideEdit } = this.props;
 
+    updatePostFn( id, text);
+    hideEdit();
   }
 
   render() {
     // More destructuring!
     const { hideEdit } = this.props;
     const { text } = this.state;
+
+    console.log('this.props', this.props)
 
     return (
       <section className="Edit__parent">
